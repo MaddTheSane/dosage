d3020c4abac38b6d2d90f12f10539b2b440d5b14
LesleyWheat <wheatd@mcmaster.ca>
2018-04-16 23:32:53 -0400

    Added support for mac and linux. Note: force qutting scripts may cause headless windows to stay open, leading to memory leak

--- dosagelib/webdriver.py
+++ dosagelib/webdriver.py
@@ -1,6 +1,7 @@
 #Webdriver file
 import sys
 import os
+import platform
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 
@@ -21,7 +22,19 @@ class ResponseMimic:
     self.status_code= 200
 
 def startDriver():
-  driverPath =os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)),os.pardir))  + '\scripts\chromedriver.exe'
+  sys = platform.system()
+  if sys == 'Windows':
+    driverProgram = '\chromedriver_win3.exe'
+  if sys == 'Darwin':
+    #check correct
+    driverProgram = '\chromedriver_mac64'
+  if sys == 'Linux':
+    driverProgram = '\chromedriver_linux64'
+  else:
+    raise Exception('Unidentifed system type by platform.system()')
+
+  #Chrome must be installed in default location
+  driverPath =os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)),os.pardir))  + '\dosagelib\chromedriver' +driverProgram
   optionsC = webdriver.ChromeOptions()
   optionsC.add_argument('headless')
   optionsC.add_argument('--log-level=3')
